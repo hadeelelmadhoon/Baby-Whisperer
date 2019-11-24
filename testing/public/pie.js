10d89e8bf8c5212951513b8e348526f036d24128
Hadeel Elmadhoon <helmadh@uwo.ca>
2019-11-24 01:21:01 -0500

    add twilio

--- testing/public/pie.js
+++ testing/public/pie.js
@@ -30,15 +30,21 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 
     var data = google.visualization.arrayToDataTable([
-        ['Effort', 'Amount given'],
-        ['My all', 100],
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
     ]);
 
     var options = {
         pieHole: 0.5,
         height: 600,
         width: 700,
+        pieHole: 0.4,
         backgroundColor: 'transparent',
+        colors: ['#88BDBC', '#c8dad3', '#F4976C', '#ffdc82', '#E85A4F'],
         pieSliceTextStyle: {
             color: 'black',
         },
